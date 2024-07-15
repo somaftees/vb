070570e28c5dd9fdaacafa820e3d4bf5cf31dc6a
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-07-15 20:05:36 +0200

    yeeeeeeeeeaaaa

---
+++ circle/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(343, 280)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(329, 93)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(329, 143)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(329, 176)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 3
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(329, 219)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 4
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(133, 33)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(125, 62)
+        PictureBox1.TabIndex = 5
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PictureBox1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ circle/WinFormsApp1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Dim a, b, c, d, sm, t As Integer
+    Dim ta, tb, tc, td As Single
+    Dim gg As Graphics
+    Dim br1 As New SolidBrush(Color.Red)
+    Dim br2 As New SolidBrush(Color.Blue)
+    Dim br3 As New SolidBrush(Color.Green)
+    Dim br4 As New SolidBrush(Color.Yellow)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        c = Val(TextBox3.Text)
+        d = Val(TextBox4.Text)
+        sm = a + b + c + d
+        ta = a / sm * 360
+        tb = b / sm * 360
+        tc = c / sm * 360
+        td = d / sm * 360
+        t = 0
+        gg = PictureBox1.CreateGraphics
+        gg.Clear(Color.White)
+        Dim myrect As New Rectangle(0, 0, PictureBox1.Width, PictureBox1.Height)
+        gg.FillPie(br1, myrect, t, ta)
+        t += ta
+        gg.FillPie(br2, myrect, t, tb)
+        t += tb
+        gg.FillPie(br3, myrect, t, tc)
+        t += tc
+        gg.FillPie(br4, myrect, t, td)
+        t += td
+
+    End Sub
+End Class
